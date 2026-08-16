426ba0f1f4a14002f34b31a75edca336024ebf0d
Nihir Kothari <nihirkothari2007@gmail.com>
2026-08-16 16:21:31 +0530

    pract.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// var maths = Number(prompt("Enter your marks of maths"))
+// var phy = Number(prompt("Enter your marks of phy"))
+// var chem = Number(prompt("Enter your marks of chem"))
+
+// var total_marks = maths + phy + chem
+// var percentage = (total_marks / 300) * 100
+
+// console.log('Percentage: ' + percentage + '%')
+
+// if (percentage >= 75) {
+//     console.log('pass')
+// } else {
+//     console.log('fail')
+// }
+
+// var a = 10;
+// console.log(a);
+
