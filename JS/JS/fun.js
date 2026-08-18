2dd6af7242724375330b3dfb1ec3460aa7bb0fc9
Nihir Kothari <nihirkothari2007@gmail.com>
2026-08-18 19:00:02 +0530

    Add files via upload

---
+++ JS/JS/fun.js
@@ -0,0 +1,49 @@
+
+// function Num18(a){
+//     console.log("Virat Kohli",a);
+// }
+// Num18(18)
+
+//WRITE ALL THE WAYS OF PRINTING FUNCTION
+
+//FUNCTION DECLARATION
+
+// function play(){
+//     console.log("Playing");
+// }
+
+// play()
+
+//FUNTION EXPRESSION
+
+// var act = function(){
+//     console.log("Acting");
+// }
+
+// act()
+
+// ARROW FUN.
+
+// var add = (a,b) => {
+//     return (a+b)
+// }
+// console.log(add(8, 10));
+
+// second way of arrow fun
+// also called as single line funtion
+// var sub=(a,b) => a-b; console.log(sub(28,10));
+
+
+
+// IIFE(Immodeate Invoked Function)
+
+// (function(){
+//     console.log("THIS IS IIFE");
+// })()
+
+// ONE MORE SHORTER WAY OF IIFE
+
+// (() => console.log('HELLOWWW'))()
+
+
+
